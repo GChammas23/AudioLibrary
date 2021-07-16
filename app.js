5ef852277c28d95ec94df286d6dafdf25f2c7ce4
GChammas23 <69366211+GChammas23@users.noreply.github.com>
2021-07-16 16:19:37 +0300

    Added some middleware for server

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const server = http.createServer(app);
-const port = process.env.PORT || 3001;
+//SETUP MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+//CREATE SERVER
+const server = http.createServer(app);
+const PORT = 3001;
+
+//ADD LISTENER TO PORT
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
