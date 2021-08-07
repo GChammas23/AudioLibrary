5d5d37cd69de4fcf3512d66ce11c21d645a6dfb7
Gabriel Chammas <gabychammas23@gmail.com>
2021-08-07 23:23:50 +0300

    Removed try catch from track services

--- Track/track.services.js
+++ Track/track.services.js
@@ -12,130 +12,108 @@ exports.addTrackService = async (req, res) => {
   });
 
   //Save the created object in DB
-  try {
-    const result = await track.save();
-    return result._id;
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  const result = await track.save();
+  return result._id;
 };
 
 exports.getTracksService = async () => {
-  try {
-    const result = await Track.find();
+  const result = await Track.find();
 
-    return result;
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  return result;
 };
 
 exports.getTrackBySingerService = async (req, res) => {
-  try {
-    //Find the album with the specified singer
-    const result = await Track.find({ singer: req.params.singer });
+  //Find the album with the specified singer
+  const result = await Track.find({ singer: req.params.singer });
 
-    return result;
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  return result;
 };
 
 exports.updateTrackByIdService = async (req, res) => {
   //Find the track by its id and update it with the new values
-  try {
-    const track = await Track.findById(req.params.id);
+  const track = await Track.findById(req.params.id);
 
-    if (track) {
-      //Track found, now update it
-      await Track.updateOne(
-        { _id: req.params.id },
-        {
-          $set: {
-            name: req.body.name,
-            singer: req.body.singer,
-            categoryId: req.body.categoryId,
-            albumId: req.body.albumId,
-          },
+  if (track) {
+    //Track found, now update it
+    await Track.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          name: req.body.name,
+          singer: req.body.singer,
+          categoryId: req.body.categoryId,
+          albumId: req.body.albumId,
         },
-        { omitUndefined: true }
-      );
+      },
+      { omitUndefined: true }
+    );
 
-      return true; //Update done
-    } else {
-      return false; //Error in update
-    }
-  } catch (err) {
-    throw new Error(err.message);
+    return true; //Update done
+  } else {
+    return false; //Error in update
   }
 };
 
 exports.deleteTrackByIdService = async (req, res) => {
   //Delete track by id and return response
-  try {
-    //First try to find the track
-    const track = await Track.findById(req.params.id);
 
-    if (track) {
-      //Track found, now delete it
-      await Track.deleteOne({ _id: req.params.id });
+  //First try to find the track
+  const track = await Track.findById(req.params.id);
 
-      return true; //Deleted track
-    } else {
-      //No track found
-      return false;
-    }
-  } catch (err) {
-    throw new Error(err.message);
+  if (track) {
+    //Track found, now delete it
+    await Track.deleteOne({ _id: req.params.id });
+
+    return true; //Deleted track
+  } else {
+    //No track found
+    return false;
   }
 };
 
 exports.getSortedTrackService = async (req, res) => {
   //Get all songs in album with given category
-  try {
-    //Check if we have a category id sent in the request
-    if (Object.keys(req.query).length > 0) {
-      //Check for album first
-      const album = await Album.findById(req.params.albumId);
+  
+  //Check if we have a category id sent in the request
+  if (Object.keys(req.query).length > 0) {
+    //Check for album first
+    const album = await Album.findById(req.params.albumId);
 
-      if (album) {
-        //Album found, now check category
-        const category = await Category.findById(
-          req.query[Object.keys(req.query)[0]]
-        );
+    if (album) {
+      //Album found, now check category
+      const category = await Category.findById(
+        req.query[Object.keys(req.query)[0]]
+      );
 
-        if (category) {
-          //Category found, now find tracks
-          const tracks = await Track.find({
-            albumId: req.params.albumId,
-            categoryId: req.query[Object.keys(req.query)[0]],
-          });
-
-          return tracks;
-        } else {
-          //Category not found
-          return;
-        }
-      } else {
-        //Album not found
-        return;
-      }
-    } else {
-      //Find album first
-      const album = await Album.findById(req.params.albumId);
-
-      if (album) {
-        //Album found, now get all songs and return them in response
-
-        const tracks = await Track.find({ albumId: req.params.albumId });
+      if (category) {
+        //Category found, now find tracks
+        const tracks = await Track.find({
+          albumId: req.params.albumId,
+          categoryId: req.query[Object.keys(req.query)[0]],
+        });
 
         return tracks;
       } else {
-        //Album not found
+        //Category not found
         return;
       }
+    } else {
+      //Album not found
+      return;
     }
-  } catch (err) {
-    throw new Error(err.message);
+  } else {
+    //Find album first
+    const album = await Album.findById(req.params.albumId);
+
+    if (album) {
+      //Album found, now get all songs and return them in response
+
+      const tracks = await Track.find({ albumId: req.params.albumId });
+
+      return tracks;
+    } else {
+      //Album not found
+      return;
+    }
   }
 };
